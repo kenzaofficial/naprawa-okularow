e5c0ff33072ceb671b6aa04a3f8c189d5ac3dd60
kenza <henkerofficial@gmail.com>
2024-11-22 23:47:20 +0200

    meta tags were deleted

--- app.vue
+++ app.vue
@@ -4,39 +4,12 @@
   </v-layout>
 </template>
 
-<script setup>
+<script>
 import VHero from "./components/blocks/v-hero/VHero.vue";
 import VLayout from "./components/v-layout/VLayout.vue";
 
-definePageMeta({
-  title: "Naprawa Okularów - Profesjonalna Usługa | Wroclaw",
-  meta: [
-    {
-      name: "description",
-      content:
-        "Profesjonalna naprawa okularów we Wroclawiu. Szybka i dokładna obsługa.",
-    },
-    {
-      name: "keywords",
-      content: "naprawa okularów, serwis okularów, naprawa oprawek, pilna naprawa okularów",
-    },
-    { name: "author", content: "Naprawa Okularów" },
-    { name: "robots", content: "index, follow" },
-    {
-      property: "og:title",
-      content: "Naprawa Okularów - Profesjonalna Usługa | Wroclaw",
-    },
-    {
-      property: "og:description",
-      content:
-        "Profesjonalna naprawa okularów w Twoim mieście. Szybka i dokładna obsługa.",
-    },
-    { property: "og:type", content: "website" },
-    { property: "og:url", content: "https://naprawa-okularow.online" },
-    {
-      property: "og:image",
-      content: "https://www.twojastrona.pl/og-image.jpg",
-    },
-  ],
-});
+export default {
+  name: "HomePage",
+  components: { VHero, VLayout},
+};
 </script>

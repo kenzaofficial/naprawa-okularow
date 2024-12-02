7c4f7a76342939608a556e81699aa61a2203d2d1
henker <henkerofficial@gmail.com>
2024-12-03 01:20:05 +0200

    service card was created alpha version

--- app.vue
+++ app.vue
@@ -1,24 +1,56 @@
 <template>
   <v-layout>
+    <v-container>
+      <ul class="service-list">
+        <li>
+          <service-card />
+        </li>
+        <li>
+          <service-card />
+        </li>
+        <li>
+          <service-card />
+        </li>
+        <li>
+          <service-card />
+        </li>
+      </ul>
+    </v-container>
+
     <v-hero />
+    <form-client />
   </v-layout>
 </template>
 
 <script>
+import VContainer from "./components/atoms/v-container/VContainer.vue";
 import VHero from "./components/blocks/v-hero/VHero.vue";
+import ServiceCard from "./components/atoms/service-card/ServiceCard.vue";
 import VLayout from "./components/v-layout/VLayout.vue";
+import FormClient from "./components/forms/form-client/FormClient.vue";
 
 export default {
   name: "HomePage",
-  components: { VHero, VLayout },
+  components: { VHero, VLayout, FormClient, ServiceCard, VContainer },
   head() {
     return {
       title: "Главная страница - Мой сайт",
       meta: [
-        { name: "description", content: "Добро пожаловать на главную страницу моего сайта." },
+        {
+          name: "description",
+          content: "Добро пожаловать на главную страницу моего сайта.",
+        },
         { name: "keywords", content: "главная, сайт, SEO, пример" },
       ],
     };
   },
 };
 </script>
+<style>
+.service-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
+}
+.service-list li {
+}
+</style>
---
+++ components/atoms/service-card/ServiceCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="service-card">
+    <img
+      class="service-card__img"
+      src="../../../assets/images/zoom.jpeg"
+      alt="example_01"
+    />
+    <div class="service-card__text">
+        <h7 class="service-card__title">
+            Spawanie
+        </h7>
+        <span class="service-card__subtitle">mini opis</span>
+        <p class="service-card__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima vero voluptas velit, facilis, repellendus harum illo dolor officiis eum unde similiq
+        </p>
+    </div>
+  </div>
+</template>
+<style>
+.service-card {
+}
+
+.service-card__img {
+    max-width: 100%;
+}
+
+.service-card__text{
+    padding: 20px;
+    background-color: blueviolet;
+    border-radius: var(--default-radius);
+}
+</style>

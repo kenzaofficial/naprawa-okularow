e28c069dd522b1feb0d7d88ab5288937f61bdec5
henker <henkerofficial@gmail.com>
2025-02-19 14:17:37 +0100

    meta tags were moved from nuxt confit to its page

--- head.ts
+++ head.ts
@@ -1,5 +1,4 @@
 export const head = {
-    title: "Naprawa Okularów",
     htmlAttrs: {
         lang: 'pl'
     },
@@ -11,25 +10,8 @@ export const head = {
       ],
     meta: [
         {
-            name: "description",
-            content: "Naprawa okularów – Wrocław, Warszawa, Kraków, Gdańsk, Szczecin, Poznań, Łódź, Lublin, Katowice i cała Polska! Naprawiamy wszystkie uszkodzenia okularów. Odpowiadamy natychmiast!"
-        },
-        {
-            name: "keywords",
-            content:
-                "Naprawa okularów Wroclaw, Naprawa okularów, Serwis okularow, Ekpresowa naprawa okularów, szybka naprawa okularów, pilna naprawa okularów",
-        },
-        {
-            name: "author",
-            content: "Naprawa okularów",
-        },
-        {
             name: "viewport",
             content: "width=device-width, initial-scale=1.0",
-        },
-        {
-            "http-equiv": "Content-Language",
-            content: "pl",
         },
         {
             name: "robots",
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,12 +5,27 @@
 </template>
 
 <script setup lang="ts">
-import {definePageMeta} from "#imports";
+import {definePageMeta, useHead} from "#imports";
 import VHero from "~/components/blocks/v-hero/VHero.vue";
 import OtherCity from "~/components/blocks/other-city/OtherCity.vue";
 import VServices from "~/components/blocks/v-services/VServices.vue";
 
 definePageMeta({
-  layout: 'default'
-})
+  layout: "default",
+});
+
+useHead({
+  title: "Naprawa Okularów Wrocław - Serwis Okularów",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Pilna naprawa okularów we Wrocławiu. Szybka i profesjonalna naprawa okularów. Wymiana zawiasów, lutowanie oprawek, naprawa flexów i inne usługi. Skontaktuj się z nami!",
+    },
+    {
+      name: "author",
+      content: "Pilna Naprawa Okularów",
+    },
+  ],
+});
 </script>

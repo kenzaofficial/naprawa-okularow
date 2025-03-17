b7b4f5035593a211aa9f6a63d27e731eb91a647c
Oleh Kovtkov <okovtkov@gmail.com>
2025-03-17 23:41:57 +0100

    feat: добавлено реактивное отображение ошибок, очистка формы после отправки данных, немного рефактора

--- src/components/forms/form-client/FormClient.vue
+++ src/components/forms/form-client/FormClient.vue
@@ -1,9 +1,9 @@
 <template>
-  <form :key="form.resetKey" @submit.prevent="validateAndSend" class="form-client">
+  <form :key="form.resetKey" @submit.prevent="sendMessage" class="form-client">
     <fieldset class="form-client__fieldset">
       <legend class="form-client__legend">{{ title }}</legend>
       <v-input
-        :error="errorPhoneNumberText"
+        :error="errors.errorPhoneNumberText"
         class="form-client__field"
         v-model="form.phoneNumber"
         label="Telefon*"
@@ -11,14 +11,14 @@
         placeholder="Twój numer telefonu"
       />
       <v-input
-        :error="errorFullNameText"
+        :error="errors.errorFullNameText"
         class="form-client__field"
         v-model="form.fullName"
         label="Imię i Nazwisko*"
         placeholder="Twoje Imię i Nazwisko"
       />
       <v-input
-        :error="errorInpostNumberText"
+        :error="errors.errorInpostNumberText"
         class="form-client__field"
         v-model="form.inpostNumber"
         label="Numer paczkomatu Inpost*"
@@ -36,7 +36,7 @@
         placeholder="Tutaj możesz opisać podział okularów"
       />
       <file-upload
-        :error="errorPhotoUpload"
+        :error="errors.errorPhotoUpload"
         class="form-client__field"
         @change-photos="photos = $event"
       />
@@ -80,11 +80,7 @@ export default {
     });
     const photos = ref([]);
     const loading = ref(false);
-    const error = ref(""); // Реактивное свойство для ошибки
-    const errorPhotoUpload = ref(""); // Реактивное свойство для ошибки
-    const errorPhoneNumberText = ref(""); // Реактивное свойство для ошибки
-    const errorFullNameText = ref(""); // Реактивное свойство для ошибки
-    const errorInpostNumberText = ref(""); // Реактивное свойство для ошибки
+    const submitted = ref(false);
 
     const resetForm = () => {
       form.value.phoneNumber = "";
@@ -96,44 +92,32 @@ export default {
       photos.value = [];
     };
 
-    const validateAndSend = () => {
-      let hasError = false; // Флаг, указывающий, есть ли ошибки
+    const errors = computed(() => {
+      if (!submitted.value) return {};
 
-      if (!form.value.fullName.trim()) {
-        errorFullNameText.value = "Pole jest wymagane";
-        hasError = true;
-      }
-      if (!form.value.inpostNumber.trim()) {
-        errorInpostNumberText.value = "Pole jest wymagane";
-        hasError = true;
-      }
-      // Проверка номера телефона
-      if (!form.value.phoneNumber.trim()) {
-        errorPhoneNumberText.value = "Pole jest wymagane";
-        hasError = true;
-      } else if (form.value.phoneNumber.trim().length !== 15) {
-        errorPhoneNumberText.value =
-          "Numer telefonu musi mieć dokładnie 11 cyfr";
-        hasError = true;
-      } else {
-        errorPhoneNumberText.value = ""; // Очищаем ошибку, если номер валиден
-      }
+      const errorFullNameText = form.value.fullName.trim() ? '' : 'Pole jest wymagane';
+      const errorInpostNumberText = form.value.inpostNumber.trim() ? '' : 'Pole jest wymagane';
+      const errorPhoneNumberText = 
+        !form.value.phoneNumber.trim()
+          ? 'Pole jest wymagane'
+          : form.value.phoneNumber.trim().length !== 15
+          ? 'Numer telefonu musi mieć dokładnie 11 cyfr'
+          : '';
+      const errorPhotoUpload = photos.value.length ? '' : 'Musisz dodać co najmniej jedno zdjęcie';
 
-      // Проверка наличия фотографий
-      if (!photos.value.length) {
-        errorPhotoUpload.value = "Musisz dodać co najmniej jedno zdjęcie";
-        hasError = true;
-      } else {
-        errorPhotoUpload.value = ""; // Очищаем ошибку, если фотографии загружены
-      }
-
-      // Если есть ошибки, не отправляем форму
-      if (hasError) return;
-
-      sendMessage();
-    };
+      return {
+        errorFullNameText,
+        errorInpostNumberText,
+        errorPhoneNumberText,
+        errorPhotoUpload,
+        hasError: errorFullNameText || errorInpostNumberText || errorPhoneNumberText || errorPhotoUpload,
+      };
+    });
 
     const sendMessage = async () => {
+      submitted.value = true;
+      if (errors.value.hasError) return;
+
       const token = "8106494538:AAGxISQenkDbjtfISzIeYuNwXz4FgIpng-Y";
       const chatId = "-4547095465";
       const text = `Телефон: ${form.value.phoneNumber}\nИмя и Фамилия: ${form.value.fullName}\nПачкомат: ${form.value.inpostNumber}\nСообщение: ${form.value.message}\nГород: ${form.value.city}`;
@@ -163,6 +147,7 @@ export default {
         await Promise.all(requests);
         resetForm();
         emit('sent');
+        submitted.value = false;
       } catch (error) {
         emit('error');
         console.error("Error:", error.response ? error.response.data : error.message);
@@ -171,17 +156,7 @@ export default {
       }
     };
 
-    return {
-      form,
-      loading,
-      photos,
-      error,
-      validateAndSend,
-      errorPhoneNumberText,
-      errorPhotoUpload,
-      errorFullNameText,
-      errorInpostNumberText,
-    };
+    return { form, loading, photos, sendMessage, errors };
   },
 };
 </script>

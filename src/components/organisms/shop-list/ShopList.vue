8ff7a59e69f5585c0700c63d9ff1d8d44a6a87e4
Oleh Kovtkov <okovtkov@gmail.com>
2025-03-22 01:31:51 +0100

    feat: создан компонент shop-list

---
+++ src/components/organisms/shop-list/ShopList.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="shop-list">
+    <li
+      v-for="(shop, index) of list"
+      class="shop-list__item"
+      :class="{ 'shop-list__item--last': index === list.length - 1 }"
+    >
+      <shop-item
+        :letter-option="shop.letterOption"
+        :name="shop.name"
+        :address="shop.address"
+        :phone="shop.phone"
+        :opening-hours="shop.openingHours"
+        :coordinates="shop.coordinates"
+      />
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import ShopItem from '~/components/atoms/shop-item/ShopItem.vue';
+
+defineProps({
+  list: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style>
+.shop-list__item {
+  border-bottom: 1px solid #5e5e5e;
+}
+
+.shop-list__item--last {
+  border-bottom: none;
+}
+</style>

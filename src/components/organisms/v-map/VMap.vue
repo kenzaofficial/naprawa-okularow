bd5095c9b5be74ef24e1fb8358044838835e6fd4
Oleh Kovtkov <okovtkov@gmail.com>
2025-03-25 22:09:49 +0100

    fix: исправлена работа приближения карты на пк

--- src/components/organisms/v-map/VMap.vue
+++ src/components/organisms/v-map/VMap.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="map" />
+  <div
+    class="v-map"
+    id="map"
+    @mouseover="isMouseOnMap = true"
+    @mouseleave="isMouseOnMap = false"
+    @mousedown="isShowNotification = false"
+    @wheel="onWheel">
+    <div class="v-map__notification" :class="{ 'v-map__notification--shown': isShowNotification }">
+      <p>Naciśnij CTRL i przewiń, by przybliżyć mapę</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -21,6 +31,9 @@ const props = defineProps({
 });
 
 const map = ref(null);
+const isMouseOnMap = ref(false);
+const isShowNotification = ref(false);
+let ctrlPressed = false;
 
 const updateMarkerStyle = (marker, newValue) => {
   const scaleValue = 'scale(1.5) translateX(-4px) translateY(-14px)';
@@ -28,6 +41,48 @@ const updateMarkerStyle = (marker, newValue) => {
   const newTransformValue = newValue ? scaleValue : '';
   marker.style.transition = 'transform 0.1s linear';
   marker.style.transform = transformValue + newTransformValue;
+};
+
+let timeoutId = -1;
+const onWheel = () => {
+  if (timeoutId !== -1) {
+    clearTimeout(timeoutId);
+    timeoutId = -1;
+  }
+
+  if (ctrlPressed) return;
+  isShowNotification.value = true;
+
+  timeoutId = setTimeout(() => {
+    isShowNotification.value = false;
+  }, 3000);
+};
+
+const addZoomListeners = () => {
+
+  // Включаем зум карты при нажатом Ctrl
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Control') {
+      ctrlPressed = true;
+      isShowNotification.value = false;
+      map.value.scrollWheelZoom.enable();
+    }
+  });
+
+  // Отключаем зум карты при отпускании Ctrl
+  document.addEventListener('keyup', (event) => {
+    if (event.key === 'Control') {
+      ctrlPressed = false;
+      map.value.scrollWheelZoom.disable();
+    }
+  });
+
+  // Блокируем зум страницы при зажатом Ctrl над картой
+  document.addEventListener('wheel', (event) => {
+    if (ctrlPressed && isMouseOnMap.value) {
+      event.preventDefault(); // Останавливаем зум страницы
+    }
+  }, { passive: false });
 };
 
 watch(() => props.activeMarker, (newValue, oldValue) => {
@@ -42,7 +97,9 @@ watch(() => props.activeMarker, (newValue, oldValue) => {
 onMounted(() => {
   import('leaflet').then((L) => {
     // Инициализация карты
-    map.value = L.map("map").setView([props.centerCoordinates.lat, props.centerCoordinates.lng], 12);
+    map.value =
+      L.map('map', { scrollWheelZoom: false })
+        .setView([props.centerCoordinates.lat, props.centerCoordinates.lng], 12);
   
     // Подключаем слой OpenStreetMap
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -51,7 +108,6 @@ onMounted(() => {
     // Добавляем маркеры на карту
     props.markers.forEach(({ lat, lng, text, id }) => {
       const customIcon = L.divIcon({
-        id: 'kek',
         className: `v-map__marker v-map__marker--${id}`,
         html: `<div class="custom-marker" />`,
         iconSize: [25, 40],
@@ -63,12 +119,15 @@ onMounted(() => {
         .addTo(map.value)
         .bindPopup(text);
     });
+
+    addZoomListeners();
   });
 });
 </script>
 
 <style>
 #map {
+  position: relative;
   width: 100%;
   height: 400px;
   margin: 0 auto;
@@ -77,5 +136,27 @@ onMounted(() => {
 
 .v-map__marker {
   background-image: url('./marker-icon.svg');
+}
+
+.v-map__notification {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  background-color: #00000077;
+  color: #fff;
+  font-size: 22px;
+  transition: opacity 0.2s linear;
+  opacity: 0;
+}
+
+.v-map__notification--shown {
+  z-index: 401;
+  opacity: 1;
 }
 </style>
